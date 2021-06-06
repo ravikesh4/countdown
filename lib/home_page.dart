337567814abdb25ebf9d0ba431bcce764c5457b8
Ravikesh Yadav <ravikesh@netzary.com>
2021-06-06 19:28:49 +0530

    datetime-done

---
+++ lib/home_page.dart
@@ -0,0 +1,229 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Timer _timer;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+          formattedDate = DateFormat('kk:mm').format(DateTime.now());
+      });
+    });
+  }
+  void _startTimer() {
+    _counter = _counter;
+    if (_timer != null) {
+      _timer.cancel();
+    }
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_counter > 0) {
+          _counter--;
+        } else {
+          _timer.cancel();
+        }
+      });
+    });
+  }
+
+  void _pauseCountDown() {
+    _timer.cancel();
+  }
+
+  void _resetCountDown() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+
+  DateTime selectedDate = DateTime.now();
+  var differenceInDays = 0;
+  String formattedDate = DateFormat('kk:mm:ss').format(DateTime.now());
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2101));
+    DateTime dateTimeNow = DateTime.now();
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+        _counter = selectedDate
+            .difference(dateTimeNow)
+            .inSeconds;
+      });
+  }
+
+  static String formatDuration(Duration d) {
+    var seconds = d.inSeconds;
+    final days = seconds ~/ Duration.secondsPerDay;
+    seconds -= days * Duration.secondsPerDay;
+    final hours = seconds ~/ Duration.secondsPerHour;
+    seconds -= hours * Duration.secondsPerHour;
+    final minutes = seconds ~/ Duration.secondsPerMinute;
+    seconds -= minutes * Duration.secondsPerMinute;
+
+    final List<String> tokens = [];
+    if (days != 0) {
+      tokens.add('${days}d');
+    }
+    if (tokens.isNotEmpty || hours != 0) {
+      tokens.add('${hours}h');
+    }
+    if (tokens.isNotEmpty || minutes != 0) {
+      tokens.add('${minutes}m');
+    }
+    tokens.add('${seconds}s');
+
+    return tokens.join(':');
+  }
+
+  TimeOfDay _time = TimeOfDay(hour: 7, minute: 15);
+  DateTime selectedTime = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+
+  void _selectTime() async {
+    final TimeOfDay newTime = await showTimePicker(
+      context: context,
+      initialTime: _time,
+    );
+    if (newTime != null) {
+      setState(() {
+        _time = newTime;
+        print('selectd time $selectedTime');
+        print('compare time ${DateTime(_time.hour, _time.minute)}');
+        _counter = selectedTime
+            .difference(DateTime(_time.hour, _time.minute))
+            .inSeconds;
+      });
+    }
+  }
+  bool _switchValue=false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Timer App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CupertinoSwitch(
+              value: _switchValue,
+              onChanged: (value) {
+                setState(() {
+                  _switchValue = value;
+                  if(_switchValue == true) {
+                    _counter = DateTime.now().add(Duration(minutes: 20))
+                        .difference(DateTime.now())
+                        .inSeconds;
+                    _startTimer();
+                  } else {
+                    _counter = 0;
+                  }
+
+                });
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Add 20 Minutes Countdown',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+
+
+
+
+            (_counter > 0)
+                ? Text("")
+                : Text(
+              "DONE!",
+              style: TextStyle(
+                color: Colors.green,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Countdown: ${formatDuration(Duration(seconds: _counter))}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Current time: ${formattedDate}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+
+            ElevatedButton(
+              onPressed: _selectTime,
+              child: Text('SELECT TIME'),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Selected time: ${_time.format(context)}',
+            ),
+            Text("${selectedDate.toLocal()}".split(' ')[0]),
+            SizedBox(
+              height: 20.0,
+            ),
+            RaisedButton(
+              onPressed: () => _selectDate(context),
+              child: Text('Select date'),
+            ),
+
+            RaisedButton(
+              onPressed: () => _startTimer(),
+              child: Text("Start",),
+              color: Colors.blue,
+            ),
+            RaisedButton(
+              onPressed: () => _pauseCountDown(),
+              child: Text("Pause"),
+              color: Colors.yellow,
+            ),
+            RaisedButton(
+              onPressed: () => _resetCountDown(),
+              child: Text("Reset"),
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:timer/count_down_timer_page.dart';
-import 'package:timer/count_up_timer_page.dart';
+import 'package:timer/home_page.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
-    );
-  }
-}
-
-class Homepage extends StatefulWidget {
-  const Homepage({Key key}) : super(key: key);
-
-  @override
-  _HomepageState createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  bool hasTimerStopped = false;
-  @override
-  Widget build(BuildContext context) {
-    return  Container(
-      width: 60.0,
-      padding: EdgeInsets.only(top: 3.0, right: 4.0),
-      child: CountDownTimer(
-        secondsRemaining: 3620,
-        whenTimeExpires: () {
-          setState(() {
-            hasTimerStopped = true;
-          });
-        },
-        countDownTimerStyle: TextStyle(
-            color: Color(0XFFf5a623),
-            fontSize: 17.0,
-            height: 1.2),
-      ),
+      home: MyHomePage(),
     );
   }
 }
