e04e58c548169609af565108d708649eac878154
Ravikesh Yadav <ravikesh@netzary.com>
2021-06-06 13:35:10 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:slide_countdown_clock/slide_countdown_clock.dart';
+import 'package:timer/count_down_timer_page.dart';
+import 'package:timer/count_up_timer_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Counter App'),
+      home: CountDownTimerPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
-  int hours = 40;
-
-  DateTime selectedDate = DateTime.now();
-  var differenceInDays = 0;
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime.now(),
-        lastDate: DateTime(2101));
-    DateTime dateTimeNow = DateTime.now();
-    if (picked != null && picked != selectedDate)
-      setState(() {
-        selectedDate = picked;
-        differenceInDays = selectedDate.difference(dateTimeNow).inDays;
-        print(differenceInDays);
-        counter(differenceInDays);
-      });
-  }
-
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key:_scaffoldKey,
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Plugin example app'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("${selectedDate.toLocal()}".split(' ')[0]),
-            SizedBox(
-              height: 20.0,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.pinkAccent,
+                  padding: const EdgeInsets.all(4),
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () {
+                  CountUpTimerPage.navigatorPush(context);
+                },
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    'Go To CountUpTimer',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
             ),
-            RaisedButton(
-              onPressed: () => _selectDate(context),
-              child: Text('Select date'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.pinkAccent,
+                  padding: const EdgeInsets.all(4),
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () {
+                  CountDownTimerPage.navigatorPush(context);
+                },
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    'Go To CountDownTimer',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
             ),
-            Text('CLock 1'),
-            counter(differenceInDays),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        // onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  Widget counter(int differenceInDays) {
-    return SlideCountdownClock(
-
-      duration: Duration(days: differenceInDays),
-      shouldShowDays: true,
-      slideDirection: SlideDirection.Up,
-      separator: ':',
-      textStyle: TextStyle(
-        fontSize: 40,
       ),
     );
   }
